d373db129c20c69f641b6ffcca0c0288ca6fc4b2
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2012-12-10 01:39:38 +0100

    Fixed run.sh
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- run.sh
+++ run.sh
@@ -4,16 +4,17 @@ U_ID="$(id -u)"
 U_HOME="$(pwd)"
 U_ASSEMBLY="${U_HOME}/target/multibot210-assembly-1.0.jar"
 
-if [ ! -f "${ASSEMBLY}" ]; then
+if [ ! -f "${U_ASSEMBLY}" ]; then
   sbt assembly
 fi
 
+unset JAVA_OPTS
+# unset SBT_OPTS
 java \
 -Djava.security.manager \
 -Dmultibot.production=true \
 -Duser.name="${U_ID}" \
 -Duser.home="${U_HOME}" \
--noverify \
 -XX:+CMSClassUnloadingEnabled \
 -Dfile.encoding=UTF-8 \
 -XX:MaxPermSize=512m \
